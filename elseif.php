c8016f374edf885fb638ae39f045775b832f998c
M Tarek <mohiuddintarek2017@gmail.com>
2022-01-16 19:48:22 +0600

    first commit

---
+++ elseif.php
@@ -0,0 +1,56 @@
+<?php
+$month_name = 'mar';
+
+if ($month_name == 'jan')
+{
+    echo "This month is january";
+}
+else if($month_name == 'feb')
+{
+    echo "This month is February";
+}
+else if($month_name == 'mar')
+{
+    echo "This month is March";
+}
+else if($month_name == 'apr')
+{
+    echo "This month is April";
+}
+else if($month_name == 'may')
+{
+    echo "This month is May";
+}
+else if($month_name == 'jun')
+{
+    echo "This month is June";
+}
+else if($month_name == 'jul')
+{
+    echo "This month is July";
+}
+else if($month_name == 'aug')
+{
+    echo "This month is Augest";
+}
+else if($month_name == 'sep')
+{
+    echo "This month is September";
+}
+else if($month_name == 'oct')
+{
+    echo "This month is October";
+}
+else if($month_name == 'nov')
+{
+    echo "This month is November";
+}
+else if($month_name == 'dec')
+{
+    echo "This month is December";
+}
+else {
+   echo "No month exist";
+}
+
+?>
---
+++ ifloop.php
@@ -0,0 +1,26 @@
+<?php
+$name = "tarek";
+$number = 12;
+
+if($name == "Tarek")
+{
+    echo "Successfully Login";
+}
+else {
+    echo "Wrong credintential";
+}
+
+echo "<br>";
+
+if($number == 12)
+{
+    echo "The Number you type is correct";
+}
+else
+{
+    echo "The Number you type is not correct";
+}
+
+
+
+?>
